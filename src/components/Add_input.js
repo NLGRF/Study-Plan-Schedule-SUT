29a392b663b5997f2a87fdc00fc0333c5a2d031e
dtmkeng <kang081@hotmail.com>
2017-10-03 16:35:39 +0700

    sort data push to object

--- src/components/Add_input.js
+++ src/components/Add_input.js
@@ -5,12 +5,14 @@ export default class Add_input extends Component {
     constructor(){
         super();
         this.state={
-            couseID:"",
+            couseID:" ",
             error:{},
             Name:undefined,
             Credit:undefined,
             Groups:[],
-            couse:'102108'
+            couse:'102108',
+            todo:[],
+            row:[],
         }
         this.handleChange =this.handleChange.bind(this);
         this.handleSubmit =this.handleSubmit.bind(this);
@@ -51,8 +53,14 @@ if (!id || id == '') {
   fetch(`https://still-mountain-63520.herokuapp.com/api.php?id=${query}`)//103101 202109
   .then( function(response) {
     return response;
+    //console.log(response);
   })
   .then( function(response) {
+      if(response.status!==200){
+          let error={};
+          error.user="Don,t have Couse ID";
+          main.setState({error})
+      }
       setTimeout( function() {
           main.setState({
           infoStatus: 'loaded'
@@ -61,12 +69,12 @@ if (!id || id == '') {
     return response.json();
   }).then((data)=>{
       let G=data.Groups;
-      console.log(data)
+      //console.log(data)
      main.setState({
           Name:data.Name,
           Credit:data.Credit,
           Groups:data.Groups
-      })
+      });
   }).catch( function() {
       main.setState({
         infoStatus: 'error'
@@ -87,76 +95,166 @@ handleSubmit(event) {
         }else{
             console.log("Don't next,pls check you home")   
       }
+      let row=[]
+      let todo=[]
+      const {
+            Name,
+            Credit,
+            Groups,
+            infoStatus,
+        } = this.state
+        let d =this.state.Groups;
+        //console.log(d);
+        var size = Object.keys(d).length;
+        let G=1;
+        if(d!==undefined){
+        for(let i=1;i<=size;i++){
+            for(let j=0;j<= Object.keys(d[i]).length;j++){ 
+               //console.log(`key ${j}`,d[i][j]); 
+              // console.log(Object.keys(d[i]).length)
+               if((d[i][j]!==undefined&&j===0)&&Object.keys(d[i]).length===4){ //3 
+                   row.push(<div>
+                               <div onClick={this.seletCouse} key={i}> 
+                                                  <tr className="List_A" >
+                                                      <td>Groups: {G}</td>
+                                                      <td>{d[i][j].Date}:{d[i][j].Time}=>{d[i][j].Room}</td>
+                                                      <td>{d[i][j+1].Date}:{d[i][j+1].Time}=>{d[i][j+1].Room}</td>
+                                                      <td>{d[i][j+2].Date}:{d[i][j+2].Time}=>{d[i][j+2].Room}</td>
+                                                  </tr>
+                               </div>
+                           </div>
+                           );
+                           todo.push({
+                              Date:`${d[i][j].Date}${d[i][j].Time}${d[i][j].Room}=${d[i][j+1].Date}${d[i][j].Time}${d[i][j+1].Room}=${d[i][j+2].Date}${d[i][j].Time}${d[i][j+2].Room}`.trim()
+                          })
+                           //console.log(j)
+                }
+               else if((d[i][j]!==undefined&&j===0)&&Object.keys(d[i]).length===2){  //1
+                  row.push(<div>
+                                  <div onClick={this.seletCouse(i)}  key={i}>
+                                                  <tr className="List_A"  >
+                                                      <td>Groups: {G}</td>
+                                                      <td>{d[i][j].Date}:{d[i][j].Time}=>{d[i][j].Room}</td>
+                                                  </tr>
+                                  </div>
+                             </div>
+                          );
+                          todo.push({
+                              Date:`${d[i][j].Date}${d[i][j].Time}${d[i][j].Room}`.trim()
+                          })
+                          //console.log(j)
+               }
+               else if((d[i][j]!==undefined&&j===0)&&Object.keys(d[i]).length===3){//2
+                  row.push(<div>
+                                  <div onClick={this.seletCouse(i) }>
+                                                  <tr className="button List_A" >
+                                                      <td>Groups: {G}</td>
+                                                      <td>{d[i][j].Date}:{d[i][j].Time}=>{d[i][j].Room}</td>
+                                                      <td>{d[i][j+1].Date}:{d[i][j+1].Time}=>{d[i][j+1].Room}</td>
+                                                  </tr>
+                                  </div>
+                           </div>
+              );
+              //console.log(j)\
+                todo.push({
+                    Date:`${d[i][j].Date}${d[i][j].Time}${d[i][j].Room}=${d[i][j+1].Date}${d[i][j].Time}${d[i][j+1].Room}`.trim()
+                })
+               }
+            }
+            G++;
+        }
+        this.setState({row:row,todo:todo});
+      }
 }
 seletCouse=(G)=>{
     let d =this.state.Groups;
+    //console.log("HHH",G);
 }
 componentWillMount() {
     this._getWeatherInfo();
-    this.seletCouse();
+  };
+  componentDisMount() {
+    this.handleSubmit()
   };
     render() {
-        let row=[]
+        let todo=[]
         const {
               Name,
               Credit,
               Groups,
-              infoStatus
+              infoStatus,
+              row
           } = this.state
-          let d =this.state.Groups;
-          //console.log(d);
-          var size = Object.keys(d).length;
-          let G=1;
-          if(d!==undefined){
-          for(let i=1;i<=size;i++){
-              for(let j=0;j<= Object.keys(d[i]).length;j++){ 
-                 //console.log(`key ${j}`,d[i][j]); 
-                // console.log(Object.keys(d[i]).length)
-                 if((d[i][j]!==undefined&&j===0)&&Object.keys(d[i]).length===4){ //3 
-                     row.push(<div>
-                                 <div>
-                                                    <tr className="List_A" onClick={this.seletCouse}>
-                                                        <td>Groups: {G}</td>
-                                                        <td>{d[i][j].Date}:{d[i][j].Time}=>{d[i][j].Room}</td>
-                                                        <td>{d[i][j+1].Date}:{d[i][j+1].Time}=>{d[i][j+1].Room}</td>
-                                                        <td>{d[i][j+2].Date}:{d[i][j+2].Time}=>{d[i][j+2].Room}</td>
-                                                    </tr>
-                                 </div>
-                             </div>
-                             );
-                             //console.log(j)
-                  }
-                 else if((d[i][j]!==undefined&&j===0)&&Object.keys(d[i]).length===2){  //1
-                    row.push(<div>
-                                                    <tr className="List_A"  onClick={this.seletCouse} >
-                                                        <td>Groups: {G}</td>
-                                                        <td>{d[i][j].Date}:{d[i][j].Time}=>{d[i][j].Room}</td>
-                                                    </tr>
-                               </div>
-                            );
-                            //console.log(j)
-                 }
-                 else if((d[i][j]!==undefined&&j===0)&&Object.keys(d[i]).length===3){//2
-                    row.push(<div>
-                                                    <tr className="List_A" onClick={this.seletCouse} >
-                                                        <td>Groups: {G}</td>
-                                                        <td>{d[i][j].Date}:{d[i][j].Time}=>{d[i][j].Room}</td>
-                                                        <td>{d[i][j+1].Date}:{d[i][j+1].Time}=>{d[i][j+1].Room}</td>
-                                                    </tr>
-                             </div>
-                );
-                //console.log(j)
-                 }
-              }
-              G++;
-          }
-        }
-         // row.splice(3,4);
+        //   let d =this.state.Groups;
+        //   //console.log(d);
+        //   var size = Object.keys(d).length;
+        //   let G=1;
+        //   if(d!==undefined){
+        //   for(let i=1;i<=size;i++){
+        //       for(let j=0;j<= Object.keys(d[i]).length;j++){ 
+        //          //console.log(`key ${j}`,d[i][j]); 
+        //         // console.log(Object.keys(d[i]).length)
+        //          if((d[i][j]!==undefined&&j===0)&&Object.keys(d[i]).length===4){ //3 
+        //              row.push(<div>
+        //                          <div onClick={this.seletCouse} key={i}> 
+        //                                             <tr className="List_A" >
+        //                                                 <td>Groups: {G}</td>
+        //                                                 <td>{d[i][j].Date}:{d[i][j].Time}=>{d[i][j].Room}</td>
+        //                                                 <td>{d[i][j+1].Date}:{d[i][j+1].Time}=>{d[i][j+1].Room}</td>
+        //                                                 <td>{d[i][j+2].Date}:{d[i][j+2].Time}=>{d[i][j+2].Room}</td>
+        //                                             </tr>
+        //                          </div>
+        //                      </div>
+        //                      );
+        //                      todo.push({
+        //                         Date:`${d[i][j].Date}${d[i][j].Time}${d[i][j].Room}=${d[i][j+1].Date}${d[i][j].Time}${d[i][j+1].Room}=${d[i][j+2].Date}${d[i][j].Time}${d[i][j+2].Room}`.trim()
+        //                     })
+        //                      //console.log(j)
+        //           }
+        //          else if((d[i][j]!==undefined&&j===0)&&Object.keys(d[i]).length===2){  //1
+        //             row.push(<div>
+        //                             <div onClick={this.seletCouse(i)}  key={i}>
+        //                                             <tr className="List_A"  >
+        //                                                 <td>Groups: {G}</td>
+        //                                                 <td>{d[i][j].Date}:{d[i][j].Time}=>{d[i][j].Room}</td>
+        //                                             </tr>
+        //                             </div>
+        //                        </div>
+        //                     );
+        //                     todo.push({
+        //                         Date:`${d[i][j].Date}${d[i][j].Time}${d[i][j].Room}`.trim()
+        //                     })
+        //                     //console.log(j)
+        //          }
+        //          else if((d[i][j]!==undefined&&j===0)&&Object.keys(d[i]).length===3){//2
+        //             row.push(<div>
+        //                             <div onClick={this.seletCouse(i) }>
+        //                                             <tr className="button List_A" >
+        //                                                 <td>Groups: {G}</td>
+        //                                                 <td>{d[i][j].Date}:{d[i][j].Time}=>{d[i][j].Room}</td>
+        //                                                 <td>{d[i][j+1].Date}:{d[i][j+1].Time}=>{d[i][j+1].Room}</td>
+        //                                             </tr>
+        //                             </div>
+        //                      </div>
+        //         );
+        //         //console.log(j)\
+        //           todo.push({
+        //               Date:`${d[i][j].Date}${d[i][j].Time}${d[i][j].Room}=${d[i][j+1].Date}${d[i][j].Time}${d[i][j+1].Room}`.trim()
+        //           })
+        //          }
+        //       }
+        //       G++;
+        //   }
+        // }
+          //row.splice(3,4);
           //console.log(size)
-          console.log(row);
+          //console.log(row);
+          //console.log(this.state.todo);
           let data = null;
-          if (infoStatus === 'loaded') {
-              data=<div><b>{Name}  - {Credit}</b></div>
+          if (infoStatus === 'loaded'&&this.state.row.length>0) {
+              data=<div><b>{Name}  - {Credit}</b>
+                    {row}
+                    </div>
           } else if (infoStatus === 'loading') {
             data = <div className="is-loading">Loading data...</div>
           } else if (infoStatus === 'error') {
@@ -167,7 +265,7 @@ componentWillMount() {
                  <form className="field is-grouped " onSubmit={this.handleSubmit} >
                   <p className={classnames('control is-expanded',{error:!!this.state.error.user})}>
                     <input className="input" type="text" 
-                     placeholder="รายชื่อวิชา" 
+                     placeholder="Enter couse ID" 
                      onChange={this.handleChange}
                      value={this.state.couseID}
                      name='couseID'
@@ -182,7 +280,6 @@ componentWillMount() {
                 <tbody>
                     <br/>
                     {data}
-                    {row}
                 </tbody>
               </table>
             </div>
